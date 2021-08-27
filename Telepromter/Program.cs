f3f9f3b3936f3acf9cc4af8b5de8c0f9c9021d96
Gerardo Daniel Romero Rubio <gerardo.romero@globant.com>
2021-08-27 16:31:57 -0500

    Add first method

--- Telepromter/Program.cs
+++ Telepromter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Telepromter
 {
@@ -8,21 +9,53 @@ namespace Telepromter
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
             var lines = ReadFrom("LoremIpsum.txt");
             foreach (var line in lines)
             {
-                Console.WriteLine(line);
+                Console.Write(line);
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    var pause = Task.Delay(300);
+                    pause.Wait();
+                }
             }
         }
         static IEnumerable<string> ReadFrom(string file)
         {
+            var lineLength = 0;
             string line;
             using (var reader = File.OpenText(file))
             {
                 while ((line = reader.ReadLine()) != null)
                 {
-                    yield return line;
+                    var words = line.Split(' ');
+                    foreach (var word in words)
+                    {
+                        yield return word + " ";
+
+                        lineLength += word.Length + 1;
+                        if (lineLength > 70)
+                        {
+                            yield return Environment.NewLine;
+                            lineLength = 0;
+                        }
+
+                    }
+                    yield return Environment.NewLine;
+
+                }
+            }
+        }
+
+        private static async Task ShowTeleprompter()
+        {
+            var words = ReadFrom("sampleQuotes.txt");
+            foreach (var word in words)
+            {
+                Console.Write(word);
+                if (!string.IsNullOrWhiteSpace(word))
+                {
+                    await Task.Delay(200);
                 }
             }
         }
