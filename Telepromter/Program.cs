ac916177917944041a2fabdef24b7335aea986df
Gerardo Daniel Romero Rubio <gerardo.romero@globant.com>
2021-08-27 16:52:50 -0500

    Final changes

--- Telepromter/Program.cs
+++ Telepromter/Program.cs
@@ -9,18 +9,19 @@ namespace Telepromter
     {
         static void Main(string[] args)
         {
-            var lines = ReadFrom("LoremIpsum.txt");
-            foreach (var line in lines)
+            int timing = 300;
+            var file = ReadFileName("LoremIpsum.txt");
+            foreach (var line in file)
             {
                 Console.Write(line);
                 if (!string.IsNullOrWhiteSpace(line))
                 {
-                    var pause = Task.Delay(300);
+                    var pause = Task.Delay(timing);
                     pause.Wait();
                 }
             }
         }
-        static IEnumerable<string> ReadFrom(string file)
+        static IEnumerable<string> ReadFileName(string file)
         {
             var lineLength = 0;
             string line;
@@ -34,7 +35,7 @@ namespace Telepromter
                         yield return word + " ";
 
                         lineLength += word.Length + 1;
-                        if (lineLength > 70)
+                        if (lineLength > 100)
                         {
                             yield return Environment.NewLine;
                             lineLength = 0;
@@ -47,18 +48,18 @@ namespace Telepromter
             }
         }
 
-        private static async Task ShowTeleprompter()
-        {
-            var words = ReadFrom("sampleQuotes.txt");
-            foreach (var word in words)
-            {
-                Console.Write(word);
-                if (!string.IsNullOrWhiteSpace(word))
-                {
-                    await Task.Delay(200);
-                }
-            }
-        }
+        //private static async Task ShowTeleprompter()
+        //{
+        //    var words = ReadFileName("LoremIpsum.txt");
+        //    foreach (var word in words)
+        //    {
+        //        Console.Write(word);
+        //        if (!string.IsNullOrWhiteSpace(word))
+        //        {
+        //            await Task.Delay(200);
+        //        }
+        //    }
+        //}
 
     }
 }
